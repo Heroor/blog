9ffc91b6721b2ed46f804a6eb1011d9aa9de781b
ben <hero@123.com>
2017-11-06 23:45:48 +0800

    添加404页面的路由

--- src/router/index.js
+++ src/router/index.js
@@ -7,25 +7,31 @@ export default new Router({
   routes: [{
     // 主视口 呈现导航
     path: '/',
-    component:  _ => import('@/views'),
+    component:  () => import('@/views'),
     children: [{
       // home页 呈现头图、文章列表、详情页
       path: '/',
-      component: _ => import('@/views/articles'),
+      component: () => import('@/views/articles'),
       children: [
         {
           // 文章列表
           name: 'list',
           path: '/',
-          component: _ => import('@/components/article')
+          component: () => import('@/components/article')
         },
         // 文章
         {
           name: 'article',
           path: '/article-*',
-          component: _ => import('@/views/articles/2017-9-22-15-31.vue')
+          component: () => import('@/views/articles/article-details.vue')
         }
       ]
     }]
+  },{
+    path: '*',
+    name: '404',
+    component: {
+      template: '<p>404</p>'
+    }
   }]
 })
