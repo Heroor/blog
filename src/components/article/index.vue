7acc8af281ad07abeb1a3e6de7d5589bcf2b8573
ben <hero@123.com>
2017-11-06 23:44:55 +0800

    更改文章列表的跳转路径

--- src/components/article/index.vue
+++ src/components/article/index.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class="home-article-list">
-        <ul>
-            <li v-for="(item, index) in list" :key="index" class="transition-05">
-                <h3 class="t-a-c w-b-bw">
-                    <!-- <a :href="item.href" class="transition-03">{{item.title}}</a> -->
-                    <router-link :to="`article-${item.articleUuid}`" class="transition-03">{{item.title}}</router-link>
-                </h3>
-                <div class="content">
-                    <div class="right">
-                        <img class="img transition-05" :src="item.image">
-                    </div>
-                    <div class="left">
-                        <p class="desc w-b-bw">{{item.desc}}</p>
-                        <span class="date">{{item.date}}</span>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </div>
+  <div class="home-article-list">
+    <ul>
+      <li v-for="(item, index) in list" :key="index" class="transition-05">
+        <h3 class="t-a-c w-b-bw">
+          <!-- <router-link :to="`article-${item.articleUuid}`" class="transition-03"> -->
+          <router-link :to="`article-${index}`" class="transition-03">
+            {{item.title}}
+          </router-link>
+        </h3>
+        <div class="content">
+          <div class="right">
+            <img class="img transition-05" :src="item.image">
+          </div>
+          <div class="left">
+            <p class="desc w-b-bw">{{item.desc}}</p>
+            <span class="date">{{item.date}}</span>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
-export default {
+  export default {
     props: ['list']
-}
+  }
 
 </script>
 <style scoped lang="scss">
-@import './article.scss'
+  @import './article.scss';
+
 </style>
