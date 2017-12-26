7bcb23d2b756ba3180c25d6c274bea60abb11cac
fbj <fbj@cto100.com>
2017-12-26 10:13:15 +0800

    修改首页文章卡片文本溢出为break-all

--- src/components/article/index.vue
+++ src/components/article/index.vue
@@ -2,7 +2,7 @@
   <div class="home-article-list">
     <ul>
       <li v-for="(item, index) in list" :key="index" class="transition-05">
-        <h3 class="t-a-c w-b-bw">
+        <h3 class="t-a-c w-b-ba">
           <!-- <router-link :to="`article-${item.articleUuid}`" class="transition-03"> -->
           <router-link :to="`article-${index}`" class="transition-03">
             {{item.title}}
@@ -10,7 +10,7 @@
         </h3>
         <div class="content">
           <div class="left">
-            <p class="desc w-b-bw">{{item.desc}}</p>
+            <p class="desc w-b-ba">{{item.desc}}</p>
             <span class="date">{{item.date}}</span>
           </div>
           <div class="right">
