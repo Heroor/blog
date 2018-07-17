40408f504b71072287bb464444762c3f15a802a7
Heroor <2388697970@qq.com>
2018-07-17 15:08:20 +0800

    feat: 初始化404页面

---
+++ src/views/404.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  render () {
+    return (
+      <div class="t-a--c m-t--200">
+        <p>Oops! There is no page.</p>
+        <a href="/">go home</a>
+      </div>
+    )
+  }
+}
+</script>
