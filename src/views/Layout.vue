363bd90c3a6c9f4fafcc4f8876adb54df512eadc
Heroor <2388697970@qq.com>
2018-07-27 20:24:29 +0800

    feat: 增加gotop

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,6 +1,7 @@
 <script>
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import GoTop from '@/components/GoTop'
 import Container from '@/views/MainContainer'
 
 export default {
@@ -10,6 +11,7 @@ export default {
         <Header />
         <Container />
         <Footer />
+        <GoTop />
       </div>
     )
   }
