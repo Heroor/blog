d8cec2637bf6538b3299d22f5816a2af669f9bac
Heroor <2388697970@qq.com>
2018-08-03 17:50:34 +0800

    add: service worker

---
+++ static/sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'cache'
+console.log('swswswswsww')
+self.addEventListener('fetch', e => {
+  console.log('event', e)
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => {
+        if (response) {
+          return response
+        }
+        return fetch(e.request).then(response => {
+          caches.open(cacheName).then(cache => {
+            cache.put(e.request, response)
+          })
+          return response
+        })
+      })
+  )
+
+})
