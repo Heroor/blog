4fa9cfff583e1d8283191de2869e3048ab73bc47
GA_FE <123@456.com>
2020-02-22 13:47:07 +0800

    fix config for history mode

--- config/index.js
+++ config/index.js
@@ -39,6 +39,7 @@ module.exports = {
   build: {
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
+    _404: path.resolve(__dirname, '../dist/404.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
